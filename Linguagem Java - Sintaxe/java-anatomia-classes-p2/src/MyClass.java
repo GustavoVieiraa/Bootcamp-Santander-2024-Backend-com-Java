57171feddcd55d38e6f4559787621337b4075d9d
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-28 18:52:17 -0300

    Add files via upload

---
+++ Linguagem Java - Sintaxe/java-anatomia-classes-p2/src/MyClass.java
@@ -0,0 +1,42 @@
+/*                     Arquivo .java
+    Todo arquivo .java deve começar com letra MAIÚSCULA.
+    Se a palavra for composta, a segunda palavra deve também 
+    ser maiúscula, exemplo:
+        Calculadora.java, CalculadoraCientifica.java
+    
+                    Nome da classe do arquivo
+    A classe deve possuir o mesmo nome do arquivo.java
+    exemplo:
+        1 // arquivo CalculadoraCientifica.java
+            public class CalculadoraCientifica {
+
+            }
+ */
+
+public class MyClass {
+    
+    public static void main (String [] args) {
+        
+        System.out.print("Hello, worlds rsrs");
+
+            /*
+                Para declarar uma variavél em Java deve se seguir algumas regras:
+
+                    - Deve conter apenas letras, _ (underline), $ ou os números de 0 a 9
+
+                    - Deve obrigatoriamente se iniciar por uma letra (preferencialmente),
+                    _ ou $, jamais com número
+
+                    - Deve iniciar com uma letra minúscula (boa prática)
+
+                    - Não pode conter espaços
+
+                    - Não podemos usar palavras-chave da linguagem
+
+                    - O nome deve ser único dentro de um escopo
+                    
+            */        
+
+    }
+
+}
