7fd1d2c73d173b4a3e3a4f9d3679c57d36d79029
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-29 15:00:20 -0300

    Aula de Tipos e Variáveis Parte 1 - concluída com êxito.

---
+++ Linguagem Java - Sintaxe/tipos-variaveis-p1/src/App.java
@@ -0,0 +1,16 @@
+/*
+                        Objetivos do Curso
+        
+        Explorar os tipos de dados numéricos inteiros,
+        númericos decimais, lógicos, caracteres e suas
+        aplicabilidades.
+        Explicar a diferença entre definição de variáveis e constantes.
+ * 
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Tipos e Variáveis");
+    }
+}
