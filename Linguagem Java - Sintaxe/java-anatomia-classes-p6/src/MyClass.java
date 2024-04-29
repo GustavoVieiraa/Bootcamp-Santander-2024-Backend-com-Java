03afa9f904397dc5e2adff4e7a2e5cb9fc3fd3e9
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-29 14:54:36 -0300

    Anatomia das classes - parte 6 - concluída com êxito

---
+++ Linguagem Java - Sintaxe/java-anatomia-classes-p6/src/MyClass.java
@@ -0,0 +1,24 @@
+class MyClass {
+
+    public static void main(String[] args) {
+        
+        /*
+         *                          Métodos
+         
+            Os métodos deverão ser nomeados como verbos, através de uma
+            mistura de letras minúsculas e maiúsculas. Em princípio,
+            todas as letras que compôem o nome devem ser mantidas
+            em minúsculo, com exceção da primeira letra de cada palavra
+            composta a partir da segunda palavra.
+
+            somar(int n1, int n2) {}
+
+            concluirProcessamento() {}
+
+            calcularImprimir() {}
+
+         */
+
+    }
+
+}
