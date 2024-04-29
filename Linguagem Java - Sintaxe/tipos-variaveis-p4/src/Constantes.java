9194e5996a98698c9ecc3070c502693d7a9c8c38
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-29 16:57:24 -0300

    Aula de Tipos e Variáveis Parte 4 - concluída com êxito.

---
+++ Linguagem Java - Sintaxe/tipos-variaveis-p4/src/Constantes.java
@@ -0,0 +1,20 @@
+/*
+                            Constantes
+
+        As constantes são valores armazenados em memória que não
+        podem ser modificados depois de declarados. Em Java, esses
+        valores são representados pela palavra reservada final,
+        seguida do tipo.
+ */
+
+ public class Constantes {
+
+    public static void main(String[] args) {
+        
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
+    }
+
+ }
