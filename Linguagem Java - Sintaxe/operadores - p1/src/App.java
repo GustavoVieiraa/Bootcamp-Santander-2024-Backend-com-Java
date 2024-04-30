40a6ab53f6f374e0376dbe6032acfd46b8fa2711
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-04-29 21:11:53 -0300

    Aula de Operadores - Parte 1 - concluída com êxito.

---
+++ Linguagem Java - Sintaxe/operadores - p1/src/App.java
@@ -0,0 +1,13 @@
+/*
+                        Operadores
+    Objetivo do curso: Apresentar o significado dos simbolos
+    especiais de acordo com a linguagem de programção Java.
+ 
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
