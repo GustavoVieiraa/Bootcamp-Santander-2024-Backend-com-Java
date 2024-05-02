d9f020e1002711abdccec35ffa386b7468e164e3
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-05-01 23:31:20 -0300

    Aula de Operadores - Parte 3 - concluída com êxito.

---
+++ Linguagem Java - Sintaxe/operadores - p3/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = + numero;
+
+        System.out.println(numero);
+
+
+    }
+
+}
