5c3c9ebb34123f3dacc23ba240778ed7a90b9155
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-05-01 23:07:47 -0300

    Aula de Operadores - Parte 2 - concluída com êxito.

---
+++ Linguagem Java - Sintaxe/operadores - p2/src/App.java
@@ -0,0 +1,22 @@
+/*
+                            Operadores
+        Símbolos especiais que tem um significado próprio para
+        a linguagem e estão associados a determinadas operações.
+ 
+  
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        int idade = 21;
+        double salario = 2500.00;
+        String nome = "gustago";
+
+
+        System.out.println(idade);
+
+    }
+
+}
