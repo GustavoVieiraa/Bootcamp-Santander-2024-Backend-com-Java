42da129c40519b487e0e390bb800cdc351e2972c
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-05-02 16:29:50 -0300

    Aula de Operadores - Parte 4 - concluída com êxito.

---
+++ Linguagem Java - Sintaxe/operadores - p4/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        numero++;
+
+        System.out.println(numero);
+
+    }
+
+}
