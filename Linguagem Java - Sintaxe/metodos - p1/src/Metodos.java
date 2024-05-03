3522fef0bd31b1c2cdd9be3b163e123229dc7158
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-05-02 23:48:02 -0300

    Aula de Metodos - Parte 1 - concluída com êxito.

---
+++ Linguagem Java - Sintaxe/metodos - p1/src/Metodos.java
@@ -0,0 +1,31 @@
+public class Metodos {
+
+    /*
+                                Objetivo do curso
+
+            Apresentar critérios para criação de métodos,
+            parâmetors e visibilidade.
+            
+      
+     */
+
+
+    public static void main(String[] args) {
+        
+        /*
+                                Assuntos
+            
+            Qual a proposta principal do método?
+
+            Qual o tipo de retorno esperado após executar o método?
+
+            Quais os parâmetros serão necessários para execução do método?
+
+            O método possui o risco de apresentar alguma exceção?
+
+            Qual a visibilidade do método?
+         
+         */
+
+    }
+}
