7d9094a07c6de2f1908573c28b3648211755b583
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-05-07 21:23:24 -0300

    Aula de Métodos - Parte 2 - concluída com êxito.

---
+++ Linguagem Java - Sintaxe/metodos - p2/src/Metodos.java
@@ -0,0 +1,25 @@
+public class Metodos {
+
+    public static void main(String[] args) {
+        
+        
+    }
+
+    /* Alguns exemplos de métodos */
+    public double somar(int n1, int n2) {
+            return n1 + n2;
+    }
+
+
+    public void imprimir(String message) {
+        System.out.println(message);
+    }
+
+    public double dividir(int dividendo, int divisor) throws Exception{
+        return dividendo / divisor;
+    }
+
+    public void gravar(Cliente client) {}
+
+}
+
