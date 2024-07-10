e06ca95a3066561e9cc2e58094761bcce1df2dac
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-07-10 00:33:01 -0300

    ForArray na prática - concluído com êxito.

---
+++ Linguagem Java - Estruturas de Repetição/ForArray/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+    public static void main(String[] args) throws Exception {
+        
+        String alunos[] = {"Rodolfo", "Rodrigo", "Walker", "Maycon", "Eduardo"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no indice [" + i + "] é " + alunos[i]);
+        }
+
+    }
+}
