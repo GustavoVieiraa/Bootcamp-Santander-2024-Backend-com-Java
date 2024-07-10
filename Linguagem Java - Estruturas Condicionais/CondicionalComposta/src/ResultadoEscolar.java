0d8974002b51fbacb38319ffd82ec28b4c39f2c8
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-07-09 22:59:27 -0300

    Estruturas Condicionais - Composta - Concluído com êxito!

---
+++ Linguagem Java - Estruturas Condicionais/CondicionalComposta/src/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+public class ResultadoEscolar {
+
+    /*          Condicional Simples           */
+
+    public static void main(String[] args) throws Exception {
+        
+            int nota = 6;
+
+            if (nota >= 7) {
+                System.out.println("Aprovado!");
+            }
+            else {
+                System.out.println("Reprovado!");
+            }
+
+    }
+}
