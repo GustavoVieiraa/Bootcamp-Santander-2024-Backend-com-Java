d33873d76adb1f929ee27683ae03c394fdee36a9
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-07-10 00:24:21 -0300

    For na prática - Concluído com êxito!

---
+++ Linguagem Java - Estruturas de Repetição/For/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) throws Exception {
+        
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+            System.out.println("Contando carneirinhos: " + carneirinhos);
+        }
+
+    }
+}
