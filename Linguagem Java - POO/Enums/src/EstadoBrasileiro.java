bfbeec6d0d09784c4836b6fec494d686217680b7
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-07-14 03:14:40 -0300

    Enums - concluída com êxito.

---
+++ Linguagem Java - POO/Enums/src/EstadoBrasileiro.java
@@ -0,0 +1,25 @@
+// Criando o enum EstadoBrasileiro para ser usado em toda a aplicação.
+public enum EstadoBrasileiro {
+	SAO_PAULO ("SP","São Paulo"),
+	RIO_JANEIRO ("RJ", "Rio de Janeiro"),
+	PIAUI ("PI", "Piauí"),
+	MARANHAO ("MA","Maranhão") ;
+	
+	private String nome;
+	private String sigla;
+	
+	private EstadoBrasileiro(String sigla, String nome) {
+		this.sigla = sigla;
+		this.nome = nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getNomeMaiusculo() {
+		return nome.toUpperCase();
+	}
+	
+}
