c8ee4494b6bf24711869d557f9b584697736a4c7
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-07-14 17:18:20 -0300

    Encapsulamento - concluída com êxito.

---
+++ Linguagem Java - Pilares POO/Encapsulamento/src/ComputadorPedrinho.java
@@ -0,0 +1,13 @@
+/*
+ * Simulação do uso da classe MSNMessenger
+ */
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+		//abrindo MSN Messenger
+		MSNMessenger msn = new MSNMessenger();
+		
+		msn.enviarMensagem();
+		msn.receberMensagem();
+		
+	}
+}
---
+++ Linguagem Java - Pilares POO/Encapsulamento/src/MSNMessenger.java
@@ -0,0 +1,24 @@
+public class MSNMessenger {
+	public void enviarMensagem() {
+		//primeiro confirmar se esta conectado a internet
+		validarConectadoInternet();
+		
+		System.out.println("Enviando mensagem");
+		
+		//depois de enviada, salva o histórico da mensagem
+		salvarHistoricoMensagem();
+		
+		
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem");
+	}
+	
+	//métodos privadas, visíveis somente na classe
+	private void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}
+	private void salvarHistoricoMensagem() {
+		System.out.println("Salvando o histórico da mensagem");
+	}
+}
