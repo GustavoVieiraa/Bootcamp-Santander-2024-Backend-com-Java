b7ca2afd11732a6956df01be27d7c5972a74e3f1
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-07-14 02:51:50 -0300

    Getters & Setters - concluída com êxito.

---
+++ GetterAndSetters/src/escola/Aluno.java
@@ -0,0 +1,26 @@
+package escola;
+
+public class Aluno {
+		
+	private String nome;
+	private int idade;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String newNome) {
+		nome = newNome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int newIdade) {
+		this.idade = newIdade;
+	}
+	
+
+
+}
