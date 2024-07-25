2eeef90d82d063a5255ea7b363146684f2850d01
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-07-25 16:33:44 -0300

    ListPesquisa build - concluída com êxito!
    ListPesquisa build - concluída com êxito!

---
+++ Linguagem Java - Conhecendo Collections Java/ListPesquisa/src/Livro.java
@@ -0,0 +1,43 @@
+
+public class Livro {
+	//atributos
+	private String titulo;
+	private String autor;
+	private int anoPublicacao;
+	
+	
+	
+	public Livro(String titulo, String autor, int anoPublicacao) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.anoPublicacao = anoPublicacao;
+	}
+	
+	@Override
+	public String toString() {
+		return "Livro titulo = " + titulo + " '\' "
+				+ "autor = " + autor + " '\' + "
+						+ " anoPublicacao=" + anoPublicacao;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getAnoPublicacao() {
+		return anoPublicacao;
+	}
+	public void setAnoPublicacao(int anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+	
+	
+}
