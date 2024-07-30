8b605dca5463d2a564410c47012b0220e1aaf3a1
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-07-30 19:33:32 -0300

    Functional Interface - Function<T,R> - concluída com êxito.
    Functional Interface - Function<T,R> - concluída com êxito.

---
+++ Linguagem Java - Ganhando Produtividade com Stream API/Functional Interface - Function/src/FunctionExample.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/*
+ * Representa uma função que aceita um argumento do tipo T e retorna
+ * um resultado do tipo R.
+ * É utilizada para transformar ou mapear os elementos do Stream em
+ * outros valores ou tipos.
+*/
+
+public class FunctionExample {
+	
+	public static void main(String[] args) {
+		
+		// Criar uma lista de números inteiros
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 25);
+		
+		// Usar a function com expressão lambda para dobrar todos os números
+		Function<Integer, Integer> dobrar = numero -> numero * 2;
+		
+		/* Usar a função para dobrar todos os números no Stream
+		   e armazená-los em outra lista.
+		*/
+		List<Integer> numerosDobrados = numeros.stream()
+				.map(n -> n * 2)
+				.toList();
+		
+		// Imprimir a lista de números dobrados
+		numerosDobrados.forEach(System.out::println);
+		
+	}
+
+}
