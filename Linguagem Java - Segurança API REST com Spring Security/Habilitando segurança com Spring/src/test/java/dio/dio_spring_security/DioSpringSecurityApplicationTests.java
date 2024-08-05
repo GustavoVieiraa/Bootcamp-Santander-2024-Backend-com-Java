86875b9ae4d93e1968dc886df194028a1ead209c
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-05 13:04:50 -0300

    Linguagem Java - Segurança API REST com Spring Security - concluído com êxito.
    Linguagem Java - Segurança API REST com Spring Security - concluído com êxito.

---
+++ Linguagem Java - Segurança API REST com Spring Security/Habilitando segurança com Spring/src/test/java/dio/dio_spring_security/DioSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.dio_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
