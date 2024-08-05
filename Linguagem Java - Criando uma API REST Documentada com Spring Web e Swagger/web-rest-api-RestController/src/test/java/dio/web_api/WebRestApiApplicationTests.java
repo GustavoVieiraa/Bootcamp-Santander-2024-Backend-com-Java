fdfee939174a72b048eb8ed48327884825da9e62
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-05 09:27:32 -0300

    web-rest-api-RestController - concluído com êxito.
    web-rest-api-RestController - concluído com êxito.

---
+++ Linguagem Java - Criando uma API REST Documentada com Spring Web e Swagger/web-rest-api-RestController/src/test/java/dio/web_api/WebRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
