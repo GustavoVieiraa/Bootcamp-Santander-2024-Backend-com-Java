2fa61c9d67e757e910d799ec6a97b87b6a648e23
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-05 07:18:12 -0300

    ConfigurationProperties - concluído com êxito.
    ConfigurationProperties - concluído com êxito.

---
+++ Linguagem Java - Spring Framework com Spring Boot/ConfigurationProperties/src/main/java/dio/springboot/app/Remetente.java
@@ -0,0 +1,39 @@
+package dio.springboot.app;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "remetente")
+public class Remetente {
+
+    private String nome;
+    private String email;
+    private List<Long> telefones;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Long> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Long> telefone) {
+        this.telefones = telefone;
+    }
+}
