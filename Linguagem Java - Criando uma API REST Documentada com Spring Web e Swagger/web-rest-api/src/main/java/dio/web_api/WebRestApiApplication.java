71575ae229e8486a92c3b956c550553a0226379b
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-05 08:10:20 -0300

    Primeiro projeto de API Rest com Spring Web - concluído com êxito.
    Primeiro projeto de API Rest com Spring Web - concluído com êxito.

---
+++ Linguagem Java - Criando uma API REST Documentada com Spring Web e Swagger/web-rest-api/src/main/java/dio/web_api/WebRestApiApplication.java
@@ -0,0 +1,13 @@
+package dio.web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRestApiApplication.class, args);
+	}
+
+}
---
+++ Linguagem Java - Criando uma API REST Documentada com Spring Web e Swagger/web-rest-api/src/main/java/dio/web_api/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package dio.web_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to My Spring Boot Web API - by gusta";
+    }
+
+}
