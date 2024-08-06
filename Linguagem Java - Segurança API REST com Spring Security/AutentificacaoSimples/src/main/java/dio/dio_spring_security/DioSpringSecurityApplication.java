d05ca21202bdff5d380f09dcef5822fedcebcaab
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-08-06 02:02:55 -0300

    AutentificacaoSimples com  Spring Security - concluído com êxito.
    AutentificacaoSimples com  Spring Security - concluído com êxito.

---
+++ Linguagem Java - Segurança API REST com Spring Security/AutentificacaoSimples/src/main/java/dio/dio_spring_security/DioSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package dio.dio_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ Linguagem Java - Segurança API REST com Spring Security/AutentificacaoSimples/src/main/java/dio/dio_spring_security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package dio.dio_spring_security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+
+
+@Configuration
+@EnableWebSecurity
+@Deprecated
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfiguration {
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user123")
+                .roles("USERS")
+                .and()
+                .withUser("admin")
+                .password("{noop}master123")
+                .roles("MANAGERS");
+    }
+}
